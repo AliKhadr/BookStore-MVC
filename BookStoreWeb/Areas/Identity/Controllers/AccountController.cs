c3736c9424be0d82b4540197ab5ce93443f1b6e9
AliKhadr <49314744+AliKhadr@users.noreply.github.com>
2026-07-23 20:21:50 +0300

    Added Register and Logout functionality

--- BookStoreWeb/Areas/Identity/Controllers/AccountController.cs
+++ BookStoreWeb/Areas/Identity/Controllers/AccountController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Models;
+using BookStore.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStoreWeb.Areas.Identity.Controllers
 {
     [Area("Identity")]
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM loginVM)
         {
             return View();
         }
@@ -15,9 +34,64 @@ namespace BookStoreWeb.Areas.Identity.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (ModelState.IsValid) 
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = registerVM.Email,
+                    Email = registerVM.Email,
+                    Name = registerVM.Name,
+                    PhoneNumber = registerVM.PhoneNumber,
+                    StreetAddress = registerVM.StreetAddress,
+                    City = registerVM.City,
+                    State = registerVM.State,
+                    PostalCode = registerVM.PostalCode
+                };
+
+                var result = await _userManager.CreateAsync(user, registerVM.Password);
+
+                if (result.Succeeded) {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home", new { area = "Customer" });
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                    //if (error.Code.StartsWith("Password"))
+                    //{
+                    //    ModelState.AddModelError("Password", error.Description);
+                    //}
+                    //// (Optional bonus) Map email errors directly to the "Email" property
+                    //else if (error.Code.Contains("Email"))
+                    //{
+                    //    ModelState.AddModelError("Email", error.Description);
+                    //}
+                    //// General identity errors stay at the top summary level
+                    //else
+                    //{
+                    //    ModelState.AddModelError(string.Empty, error.Description);
+                    //}
+                }
+            }
+            return View(registerVM);
+        }
+
         public IActionResult AccessDenied()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home", new { area = "Customer" });
         }
     }
 }
--- BookStoreWeb/Program.cs
+++ BookStoreWeb/Program.cs
@@ -18,7 +18,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequiredUniqueChars = 1;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 
